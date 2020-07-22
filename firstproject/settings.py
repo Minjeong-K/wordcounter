db3dff43d46dd424278d9da1413f2b84bfca9152
Minjeong-K <minj2058@gmail.com>
2020-07-22 18:11:35 +0900

    change host

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '82d34&m(qn+ha-7$bu(-4mumbd+v8#of3&yvcy5+a&6uw9nwq7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
